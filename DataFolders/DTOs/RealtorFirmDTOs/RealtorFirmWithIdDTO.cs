0ff264f846005608e30dafc73030dfa1dd63aba3
Samsal <43518070+baclava1001@users.noreply.github.com>
2024-05-19 17:35:16 +0200

    Activate page working. - Does not refresh properly yet.

--- DataFolders/DTOs/RealtorFirmDTOs/RealtorFirmWithIdDTO.cs
+++ DataFolders/DTOs/RealtorFirmDTOs/RealtorFirmWithIdDTO.cs
@@ -4,15 +4,9 @@ using System.ComponentModel.DataAnnotations;
 namespace DataFolders.DTOs.RealtorFirmDTOs
 {
     // Get class has id
-    public class RealtorFirmWithIdDTO
+    public class RealtorFirmWithIdDTO : RealtorFirmPostDTO
     {
         [Required]
         public int Id { get; set; }
-        [Required]
-        public string Name { get; set; } = string.Empty;
-        [Required, DisplayName("About this firm")]
-        public string RealtorFirmPresentation { get; set; } = string.Empty;
-        public string? Logotype { get; set; } = string.Empty;
-        public bool Active { get; set; }
     }
 }
